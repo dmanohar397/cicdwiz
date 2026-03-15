d5173c099f0b9c8ec8c4a34d6c0239c37f48d1d3
manohar <manohardandu397@gmail.com>
2026-03-15 18:14:08 +0530

    fix import path

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from backend.generator import generate_pipeline
+from generator import generate_pipeline
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
